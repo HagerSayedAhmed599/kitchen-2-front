6212a1d3cc037f8b151c928f380ac50acc5d4738
MahmoudAymanGomaa <ma3538463@gmail.com>
2024-11-11 16:57:48 +0200

    add all service related on supplier

--- src/app/modules/supplier/supplier.service.ts
+++ src/app/modules/supplier/supplier.service.ts
@@ -1,9 +1,38 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { environment as env } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SupplierService {
+  domain: string = env.apiUrl;
 
-  constructor() { }
+  constructor(private _HttpClient: HttpClient) { }
+
+  GetAllSupplierData(query:any): Observable<any> {
+    console.log(query);
+    let value:any = {}
+    for (const key in query) {
+      if (query[key] != null) {
+        value[key] = query[key]
+      }
+    }
+    return this._HttpClient.get(`${this.domain}supplier/GetAllclients` , {params:value})
+  }
+
+  AddSupllier(event: any) {
+    return this._HttpClient.post(`${this.domain}supplier/Addclients`,event);
+  }
+  updateSupplier(clientId: number, body:FormGroup,) {
+    return this._HttpClient.put(`${this.domain}supplier/Updateclients?clientId=${clientId}`,body);
+  }
+  getSupplierById(id: any) {
+    return this._HttpClient.get(`${this.domain}supplier/GetclientsById?id=${id}`);
+  }
+  DeleteSupplier(id: number){
+    return this._HttpClient.get(`${this.domain}supplier/Deleteclients?id=${id}`)
+  }
 }
--- src/app/modules/supplier/supplier/supplier.component.ts
+++ src/app/modules/supplier/supplier/supplier.component.ts
@@ -1,10 +1,125 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { SupplierService } from '../supplier.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-supplier',
   templateUrl: './supplier.component.html',
   styleUrls: ['./supplier.component.scss']
 })
-export class SupplierComponent {
+export class SupplierComponent implements OnInit{
+  supplierForm: FormGroup;
+  isEditMode: boolean = false;
+  suppliers: any[] = [];
+  SearchForm:FormGroup;
+  clientId: number;
+
+  constructor(private supplierService: SupplierService,private fb: FormBuilder,    private toastr: ToastrService ){
+    this.supplierForm = this.fb.group({
+      name: ['', Validators.required],
+      phone: ['', Validators.required],
+      address: [null, Validators.required],
+    });
+    this.SearchForm = this.fb.group({
+      name: [null],
+      phonenumber: [null],
+    });
+  }
+  ngOnInit(): void {
+    this.GetAllSupplierData();
+  }
+  Filter(){
+    this.GetAllSupplierData();
+  }
+  GetAllSupplierData() {
+    console.log(this.SearchForm.value);
+
+    this.supplierService.GetAllSupplierData(this.SearchForm.value).subscribe({
+      next: (res: any) => {
+        this.suppliers = res.data
+        console.log(this.suppliers);
+
+      },
+      error: (err: any) => {
+        Object.entries(err.errors).forEach(([key, value]) => {
+          // console.log(`Key: ${key}, Value: ${value}`);
+          this.toastr.error(`${value}`);
+        });
+      }
+    })
+  }
+  resetForm() {
+    this.supplierForm.reset();
+    this.clientId = null;
+  }
+  onSubmit() {
+    if (this.supplierForm.valid) {
+      const supplierData = this.supplierForm.value;
+
+      if (this.clientId) {
+        this.supplierService.updateSupplier(this.clientId, this.supplierForm.value).subscribe(
+          (res: any) => {
+            this.toastr.success(`${res.message}`);
+            this.GetAllSupplierData();
+            console.log('Supplier updated successfully');
+          },
+          (err) => {
+            console.error('Error updating supplier:', err);
+          }
+        );
+      } else {
+        this.supplierService.AddSupllier(supplierData).subscribe(
+          (res: any) => {
+            this.toastr.success(`${res.message}`);
+            this.GetAllSupplierData();
+            console.log('Supplier added successfully');
+            this.supplierForm.reset();
+          },
+          (err) => {
+            console.error('Error adding supplier:', err);
+          }
+        );
+      }
+    } else {
+      console.log('Form is invalid');
+    }
+  }
+
+  getSupplierById(id: number) {
+    this.supplierService.getSupplierById(id).subscribe((res: any) => {
+      console.log(res);
+      this.clientId = res.data.clientId;
+      console.log(this.clientId);
+
+      let supplierData = res.data;
+      console.log(supplierData);
+
+        this.supplierForm.patchValue({
+          name: supplierData.clientName,
+          phone: supplierData.clientPhone,
+          address: supplierData.clientAddress,
+          clientId: supplierData.clientId
+        });
+      },
+      (error) => {
+        console.error('Error fetching supplier data:', error);
+      }
+    );
+  }
+
+  deleteSupplier(id: number) {
+    const token = 'YOUR_JWT_TOKEN_HERE';
+    this.supplierService.DeleteSupplier(id).subscribe(
+      (response: any) => {
+        this.toastr.success(`${response.message}`);
+        this.GetAllSupplierData();
+        console.log('Client deleted successfully', response);
+      },
+      (error) => {
+        console.error('Error deleting client:', error);
+      }
+    );
+  }
 
 }
